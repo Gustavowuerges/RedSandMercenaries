2cea9346334fcbfa6d943e608c15ef897ead361e
Gustavo Fellipe Wuerges <gustavo.wuerges@cativa.com.br>
2025-09-15 12:31:22 -0300

    criado a classe mapas

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+import main.java.game.game;
+
+public class Main {
+    public static void main(String[] args) {
+        game game = new game();
+        game.ready(); // aqui roda o loop principal
+    }
+}
---
+++ src/main/java/Map/MapGrid.java
@@ -0,0 +1,21 @@
+package main.java.Map;
+
+public class MapGrid {
+
+    //inicia as variaveis em final para não ser alteradas
+    final int width, height; //tamanho e altura do mapa
+    final char[][] tiles; //cada local do mapa como exemplo: deserto, cidade, etc...
+    final boolean[][] visited; // marca se o jogador já visitou aquele local
+
+    //faz um objeto para armazenar cada local e também preencher-los 
+    MapGrid(int w , int h, char defaultTitle){
+        this.width = w; this.height = h;
+        this.tiles = new char[h][w];
+        this.visited = new boolean[h][w];
+        for (int i = 0; i < h; i++){
+            for (int k = 0; k < w; k++){
+                tiles[i][k] = defaultTitle;// preenche tudo com o terreno inicial
+            }
+        }
+    }
+}
