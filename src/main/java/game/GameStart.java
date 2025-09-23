8560cbcde0b0d22a3bd4425a188f08f974c87389
NicolasSilvaS <nicolassilvasantos00@gmail.com>
2025-09-22 21:40:28 -0300

    Still haing issues with Menu, pls don't mess with the game package for a while, i'm coding on it, cheers!

---
+++ src/main/java/game/GameStart.java
@@ -0,0 +1,10 @@
+package main.java.game;
+import main.java.Origin.*;
+import main.java.MainCharacter;
+
+public class GameStart {
+    public static void main(String[] args) {
+        JobSelection jobSelection = new JobSelection();
+        jobSelection.startSelection();
+    }
+}
